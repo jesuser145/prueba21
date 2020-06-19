7e1a39fe8d6e29f9266debc5c767b09c9d7192ad
jesuser145 <08.jesusgonzalezguerrero@gmail.com>
2020-06-19 15:59:11 +0200

    prueba1

---
+++ tabla/src/main/java/com/politecnicomalaga/App.java
@@ -0,0 +1,14 @@
+package com.politecnicomalaga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+     Multiplicacion multiplicacion=new Multiplicacion();
+        System.out.println(multiplicacion.multiplicar(2,2));
+    }
+}
